7b315fa00e2338a01d7790c903e5da83185793ae
ruhan-mri <ruhan.9mri@gmail.com>
2022-05-18 01:01:35 +0600

    java added

---
+++ app/src/main/java/iRuhan/iruhan.java
@@ -0,0 +1,27 @@
+package iRuhan;
+
+public class iruhan {
+    String name;
+    int roll;
+
+    public iruhan(String name, int roll) {
+        this.name = name;
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+}
